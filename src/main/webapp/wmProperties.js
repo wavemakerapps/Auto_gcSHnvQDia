3f16e040d6f9fca29e637402eddce2d685d7ea8e
apptest <appTest1@wavemaker.com>
2017-10-30 22:09:59 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_gcSHnvQDia",
+  "homePage" : "Main",
+  "name" : "Auto_gcSHnvQDia",
+  "platformType" : "WEB",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
